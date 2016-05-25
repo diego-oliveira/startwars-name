6a02f22220a2ab0ff708e7f0780bc87d450ea512
Diego Oliveira <oliveira.bsi@gmail.com>
2016-05-25 14:27:24 -0300

    Add unit test for all and random functions

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,27 @@ var expect = require('chai').expect
 var starWars = require('./index.js');
 
 describe('startwars-names', function() {
-  it('should work', function() {
-    expect(true).to.be.true;
+  describe('all', function() {
+    it('should be an array of strings', function() {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item){
+          return typeof item == "string";
+        });
+      }
+    });
+
+    it('should contain `Luke Skywalker`', function() {
+      expect(starWars.all).to.include('Luke Skywalker');
+    });
   });
+
+  describe('random', function() {
+    it('should return a random item from the startWars.all', function() {
+      var randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    });
+    
+  })
 });
