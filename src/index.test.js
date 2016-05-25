43e36f7e8714c6d7a9e5c76e94db7c57e54b0d0e
Diego Oliveira <diego@gistia.com>
2016-05-25 14:10:14 -0300

    Setup mocha and chai libs

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect
+var starWars = require('./index.js');
+
+describe('startwars-names', function() {
+  it('should work', function() {
+    expect(true).to.be.true;
+  });
+});
